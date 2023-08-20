2078130c52ea67c1045f47f3be8d4aae6e624b16
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-20 21:30:49 +0300

    Term Query -2

--- API/Elasticsearch/Elasticsearch.API/Repositories/ECommerceRepository.cs
+++ API/Elasticsearch/Elasticsearch.API/Repositories/ECommerceRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Elasticsearch.API.Models.ECommerceModel;
 using System.Collections.Immutable;
 
@@ -15,7 +16,16 @@ namespace Elasticsearch.API.Repositories
 
         public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
         {
-            var result = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(q => q.Term(t => t.Field("customer_first_name.keyword").Value(customerFirstName))));
+            //1.yol 
+            //var result = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(q => q.Term(t => t.Field("customer_first_name.keyword").Value(customerFirstName))));
+
+            //2.yol
+            //var result = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(q => q.Term(t => t.CustomerFirstName.Suffix("keyword"), customerFirstName)));
+
+            //3.yol
+            var termQuery = new TermQuery("customer_first_name.keyword") { Value = customerFirstName, CaseInsensitive = true };
+
+            var result = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(termQuery));
 
             foreach (var hits in result.Hits) hits.Source.Id = hits.Id;
 
